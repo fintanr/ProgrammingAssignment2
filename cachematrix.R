7f180931fd27461f94893c9820a65f00bd97d9cb
Fintan Ryan <fintanr@gmail.com>
2015-02-22 16:32:37 +0000

    Coursera rprog-011 assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -7,18 +7,16 @@
 ##
 
 ## makeCacheMatrix creates an object containing a special matrix which can 
-## be accessedvia functions in a list 
+## be accessed via functions in a list 
 ##
-## we can 
+## we call the function with an argument of a matrix, and from the returned
+## object we can
+##
 ## a) set the value of our matrix
 ## b) get the value of our matrix
 ## c) set the value of the inverse 
 ## d) get the value of the inverse
-##
-## we call the function with an argument of a matrix
-## then we set its value using the <<- operator and set our
-## cached inverse to NULL
-## 
+
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -36,10 +34,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cachesolve returns an inverse matrix from an object created by makeCacheMatrix
+## cacheSolve returns an inverse matrix from an object created by makeCacheMatrix
 ## 
 ## internally it checks the object to see if a cache exists, if it finds one it 
-## retuns that value, otherwise it calls solve() on the value of the matrix we get
+## returns that value, otherwise it calls solve() on the value of the matrix we get
 ## from the makeCacheMatrix object, calls the setinverse() function to cache the 
 ## updated value, and then returns this value
 
